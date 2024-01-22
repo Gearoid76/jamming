e16affbcb48120094b4ea42dcf7c7ab7af39f68a
Gearóid O'Ceallacháin <gocealla@gmail.com>
2024-01-22 20:45:43 +0200

    redirectToAuthCodeFlow function

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ='App'>
+      <div className ='searchBar'>Search Bar</div> 
+      <div className ='secondContainer'>
+        <div className ='containerLeft'>hello left now</div>
+        <div className ='containerRight'>Hello right now</div>
+      </div>
     </div>
   );
 }
